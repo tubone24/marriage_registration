74f2d718ce86cbaf6c7b9191f3e90029b50fd4ac
tubone24 <tubo.yyyuuu@gmail.com>
2021-05-03 01:52:13 +0900

    wip

--- src/main.py
+++ src/main.py
@@ -67,11 +67,98 @@ def husband_address_info(cfg, cc):
     return cc
 
 
+def husband_legally_domiciled_info(cfg, cc):
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["legally_domiciled_first_pos"]
+    cc.drawString(x, y, str(cfg["legally_domiciled_first"]))
+    cc.drawString(x, y - 21, str(cfg["legally_domiciled_second"]))
+    if cfg["is_banchi_legally_domiciled"]:
+        cc.ellipse(346, 473, 316, 462)
+    else:
+        cc.circle(327, 457, 6)
+    cc.drawString(x + 10, y - 44, str(cfg["head_of_person_of_legally_domiciled"]))
+    return cc
+
+
+def husband_family_info(cfg, cc):
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["father_name_pos"]
+    cc.drawString(x, y, str(cfg["father_name"]))
+    x, y = cfg["mother_name_pos"]
+    cc.drawString(x, y, str(cfg["mother_name"]))
+
+
+def wife_name_info(cfg, cc):
+    cc.setFont("ipaexm", 24)
+    x, y = cfg["last_name_pos"]
+    cc.drawString(x, y, cfg["last_name"])
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["last_name_kana_pos"]
+    cc.drawString(x, y, cfg["last_name_kana"])
+    cc.setFont("ipaexm", 24)
+    x, y = cfg["first_name_pos"]
+    cc.drawString(x, y, cfg["first_name"])
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["first_name_kana_pos"]
+    cc.drawString(x, y, cfg["first_name_kana"])
+    cc.setFont("ipaexm", 12)
+    cc.drawString(421, 569, str(cfg["birth_year"]))
+    cc.drawString(500, 569, str(cfg["birth_month"]))
+    cc.drawString(545, 569, str(cfg["birth_day"]))
+    return cc
+
+
+def wife_address_info(cfg, cc):
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["address_first_pos"]
+    cc.drawString(x, y, str(cfg["address_first"]))
+    cc.drawString(x - 12, y - 21, str(cfg["address_second"]))
+    if cfg["is_banchi_address"]:
+        cc.ellipse(502, 539, 472, 528)
+    else:
+        cc.circle(481, 523, 6)
+    cc.drawString(x + 80, y - 21, str(cfg["address_go"]))
+    cc.drawString(x + 10, y - 44, str(cfg["household_person"]))
+    cc.setFont("ipaexm", 5)
+    y -= 13
+    for text in cfg["address_apartment"].split("\n"):
+        cc.drawString(x+130, y, text)
+        y -= 5
+    return cc
+
+
+def wife_legally_domiciled_info(cfg, cc):
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["legally_domiciled_first_pos"]
+    cc.drawString(x, y, str(cfg["legally_domiciled_first"]))
+    cc.drawString(x, y - 21, str(cfg["legally_domiciled_second"]))
+    if cfg["is_banchi_legally_domiciled"]:
+        cc.ellipse(549, 473, 519, 462)
+    else:
+        cc.circle(327, 457, 6)
+    cc.drawString(x + 10, y - 44, str(cfg["head_of_person_of_legally_domiciled"]))
+    return cc
+
+
+def wife_family_info(cfg, cc):
+    cc.setFont("ipaexm", 12)
+    x, y = cfg["father_name_pos"]
+    cc.drawString(x, y, str(cfg["father_name"]))
+    x, y = cfg["mother_name_pos"]
+    cc.drawString(x, y, str(cfg["mother_name"]))
+
+
 def main():
     cc = setup()
     cfg = load_config()
     husband_name_info(cfg["husband"], cc)
     husband_address_info(cfg["husband"], cc)
+    husband_legally_domiciled_info(cfg["husband"], cc)
+    husband_family_info(cfg["husband"], cc)
+    wife_name_info(cfg["wife"], cc)
+    wife_address_info(cfg["wife"], cc)
+    wife_legally_domiciled_info(cfg["wife"], cc)
+    wife_family_info(cfg["wife"], cc)
     cc.showPage()
     cc.save()
 
